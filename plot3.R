a4dab1e0a34a3d73f01dc864e2ef2d6c1ce8708f
attuquayejames <attuquayejames@users.noreply.github.com>
2015-07-09 12:35:00 +0200

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,24 @@
+# read the data 
+mydata <- read.table("household_power_consumption.txt", header=T, sep=";")
+# convert the Date variable to Date classes in R using the as.Date() function
+mydata$Date <- as.Date(mydata$Date, format="%d/%m/%Y")
+# subset the data
+mydata <- mydata[(mydata$Date=="2007-02-01" | mydata$Date=="2007-02-02"), ]
+# convert the Global_active_power variable to numeric class in R
+mydata$Global_active_power <- as.numeric(as.character(mydata$Global_active_power))
+# transform timestamps to weekdays
+mydata <- transform(mydata, weekdays=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+# transform Sub_metering variables to numeric variables
+mydata$Sub_metering_1 <- as.numeric(as.character(mydata$Sub_metering_1))
+mydata$Sub_metering_2 <- as.numeric(as.character(mydata$Sub_metering_2))
+mydata$Sub_metering_3 <- as.numeric(as.character(mydata$Sub_metering_3))
+png(filename = "plot3.png", width = 480, height = 480)
+# generate plot3
+plot(mydata$weekdays, mydata$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(mydata$weekdays, mydata$Sub_metering_2, type="l", col="red")
+lines(mydata$weekdays, mydata$Sub_metering_3, type="l", col="blue")
+# add legend to the plot
+legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+dev.off()
+
+
